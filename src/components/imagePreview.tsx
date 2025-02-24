d621fa87e5595b07c0a857859efbc22ab0c64d03
Harris <harrisgacheru.hg@gmail.com>
2025-02-24 08:42:06 +0300

    initial commit

---
+++ src/components/imagePreview.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const ImagePreview: FC = () => {
+  return (
+    <>
+      <>image preview</>
+    </>
+  );
+};
+
+export default ImagePreview;
---
+++ src/components/previewModal.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const PreviewModal: FC = () => {
+  return (
+    <>
+      <>Preview modal</>
+    </>
+  );
+};
+
+export default PreviewModal;
